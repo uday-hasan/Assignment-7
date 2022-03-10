191e4e2f76ae932b87122d4176b2ccd4cc44c582
uday-g-ithub15 <mudayhasan@gmail.com>
2022-03-10 23:37:57 +0600

    Solve description part

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
+const loadPosts = async () => {
+  let data = await fetch('../data/posts.json');
+  posts = await data.json();
+  getData(posts);
+  console.log(posts);
+}
+const getData = (posts) => {
+  showPosts(posts)
+  getLikedPosts(posts)
+}
+
 let posts = [];
 let count = 0;
-const likedPostsId = [];
-const reportedPostsId = [];
+let likedPostsId = [];
+let reportedPostsId = [];
 
-const getLikedPosts = () => {
-  return posts.filter((post) => likedPostsId.includes(post.id));
+const getLikedPosts = (posts) => {
+  console.log(posts.filter((post) => likedPostsId.includes(post.id)));
 };
 
 const getReportedPosts = () => {
@@ -17,9 +28,6 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  console.log(id)
-  count++;
-  console.log(count);
   isLiked(id);
   // showPosts(posts);
 };
@@ -27,11 +35,12 @@ const addToLiked = (id) => {
 const reportPost = (id) => {
   reportedPostsId.push(id);
   const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
-  showPosts(remainingPosts);
+  console.log(remainingPosts);
+  // showPosts(remainingPosts);
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -71,12 +80,10 @@ const createPost = (post) => {
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
-
                 <button class="post__more-options">
                   <i class="fa-solid fa-ellipsis"></i>
                 </button>
               </div>
-
               <div class="post__content">
                 <div class="post__medias">
                   <img
@@ -86,7 +93,6 @@ const createPost = (post) => {
                   />
                 </div>
               </div>
-
               <div class="post__footer">
                 <div class="post__buttons">
                   <button class="post__button" onclick="addToLiked(${post.id})">
@@ -97,30 +103,23 @@ const createPost = (post) => {
                     <i class="fa-solid fa-comment"></i>
                   </button>
                   
-
                   <div class="post__indicators"></div>
-
                   <button class="post__button post__button--align-right" onclick="reportPost(${post.id
     })">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
-
                 <div class="post__content">${displayContent(post.description)}</div>
-
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
                       <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
                     </a>
-
                     <span>Liked by
                       <a class="post__name--underline" href="#">user123</a> and
                       <a href="#"><span id='liked-users'>73</span> others</a></span>
                   </div>
-
                   <hr/>
-
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
@@ -162,11 +161,6 @@ const displayReportedPosts = () => {
   });
 };
 
-const loadPosts = async () => {
-  let data = await fetch('../data/posts.json');
-  posts = await data.json();
-  showPosts(posts);
-  console.log(posts);
-}
+
 
 loadPosts();
