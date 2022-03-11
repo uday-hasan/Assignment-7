81c87971ff6e34f1c025f58a3bbd51788538b7e3
uday-g-ithub15 <mudayhasan@gmail.com>
2022-03-11 10:30:06 +0600

    Rearrange Script File

--- js/app.js
+++ js/app.js
@@ -1,33 +1,24 @@
-const loadPosts = async () => {
-  let data = await fetch('../data/posts.json');
-  posts = await data.json();
-  showPosts(posts);
-}
-
-let likedPostsId = [];
-let reportedPostsId = [];
+const likedPostsId = [];
+const reportedPostsId = [];
 
 const getLikedPosts = () => {
-  console.log(posts.filter((post) => likedPostsId.includes(post.id)));
   return posts.filter((post) => likedPostsId.includes(post.id));
 };
 
 const getReportedPosts = () => {
   return posts.filter((post) => reportedPostsId.includes(post.id));
 };
+
 const isLiked = (id) => {
-  return likedPostsId?.length && likedPostsId.includes(id);
+  return likedPostsId?.length && !!likedPostsId.includes(id);
 };
 
 const addToLiked = (id) => {
   likedPostsId.push(id);
-  isLiked(id);
   showPosts(posts);
 };
-//Handle multiple click on same button
 
 const reportPost = (id) => {
- 
   reportedPostsId.push(id);
   const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
   showPosts(remainingPosts);
@@ -42,27 +33,24 @@ const switchTab = (id) => {
     document.getElementById("posts").style.display = "grid";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
-  } 
-  else if (id === "liked") {
+  } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
 
     displayLikedPosts();
-     
-  }
-  else {
+  } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+
     displayReportedPosts();
-    
   }
-}
+};
 
 const createPost = (post) => {
   const image = post.image;
-  const profileImg = post.userImage;
+  const userImg = post.userImage;
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -73,14 +61,16 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${profileImg}" alt="User Picture" />
+                    <img src="${userImg}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
+
                 <button class="post__more-options">
                   <i class="fa-solid fa-ellipsis"></i>
                 </button>
               </div>
+
               <div class="post__content">
                 <div class="post__medias">
                   <img
@@ -90,33 +80,41 @@ const createPost = (post) => {
                   />
                 </div>
               </div>
+
               <div class="post__footer">
                 <div class="post__buttons">
                   <button class="post__button" onclick="addToLiked(${post.id})">
-                  <i class="fa-solid fa-heart ${isLiked(post.id) && 'text-danger'}"></i>
+                  <i class="fa-solid fa-heart ${isLiked(post.id) && "text-danger"}"></i>
                     
                   </button>
                   <button class="post__button">
                     <i class="fa-solid fa-comment"></i>
                   </button>
                   
+
                   <div class="post__indicators"></div>
+
                   <button class="post__button post__button--align-right" onclick="reportPost(${post.id
     })">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
+
                 <div class="post__content">${displayContent(post.description)}</div>
+
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
+                      <img src='${userImg}' alt="User Picture" />
                     </a>
+
                     <span>Liked by
                       <a class="post__name--underline" href="#">user123</a> and
-                      <a href="#">73 others</a>
+                      <a href="#">73 others</a></span>
                   </div>
+
                   <hr/>
+
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
@@ -143,10 +141,6 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
-  if(reportPost()){
-    alert('Already reported');
-    return;
-  }
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
@@ -156,13 +150,16 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  console.log(reportedPosts)
   reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
 };
 
-
+const loadPosts = async () => {
+  let data = await fetch('../data/posts.json');
+  posts = await data.json();
+  showPosts(posts);
+}
 
 loadPosts();
